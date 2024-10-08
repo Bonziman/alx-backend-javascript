f98f684c019cbe82d95ff1230acaabdf26b3125a
Bonziman <eloirdiwi@gmail.com>
2024-10-08 21:58:01 +0100

    get list

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,3 +1,13 @@
+// 0-get_list_students.js
+
+export function getListStudents() {
+  return [
+    { name: 'Alice', age: 25 },
+    { name: 'Bob', age: 30 },
+    { name: 'Charlie', age: 35 }
+  ];
+}
+
 function getListStudents() { // eslint-disable-line
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
